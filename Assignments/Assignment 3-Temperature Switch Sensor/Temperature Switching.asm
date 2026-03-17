0d53ba83f71b16eebe6129fadf8f35e1023b28b2
ChrisDavid2311 <davidrajaa@sonoma.edu>
2026-03-17 11:52:53 -0700

    Update Temperature Switching.asm

--- Assignments/Assignment 3-Temperature Switch Sensor/Temperature Switching.asm
+++ Assignments/Assignment 3-Temperature Switch Sensor/Temperature Switching.asm
@@ -6,7 +6,7 @@
 //
 // Dependencies: myConfigFile.inc and xc.inc
 //
-// Compiler: pic-as (V 3.10
+// Compiler: pic-as (V 3.10)
 //
 // Author: Christy Sahayaraj
 //
@@ -24,6 +24,7 @@
 // Versions:
 // V1.0: March 05, 2026 - Initial version of the program
 // V1.1: March 12, 2026 - Updated comparison logic and decimal conversion
+// V1.2: March 17, 2026 - Logic Checks & Comments 
 //-----------------------------
 
 ;----------------------------------
@@ -35,15 +36,15 @@
 ;----------------
 ; PROGRAM INPUTS
 ;----------------
-#define measuredTempInput  -5
-#define refTempInput       15
+#define measuredTempInput  -10
+#define refTempInput       50
 
 ;---------------------
 ; Definitions
 ;---------------------
 #define SWITCH   LATD,2
-#define LED0     PORTD,0
-#define LED1     PORTD,1
+#define LED0     PORTD,1
+#define LED1     PORTD,2
 
 ;---------------------
 ; Program Constants
@@ -53,31 +54,31 @@ REG11   EQU 11h
 REG01   EQU 1h
 
 ;----------------------------------
-; Main registers
+; Main Registers
 ;----------------------------------
 refTempREG               EQU 0x20
 measuredTempREG          EQU 0x21
 contREG                  EQU 0x22
 
 ;----------------------------------
-; Work registers
+; Work Registers (Hex-Dec Conversion Specific)
 ;----------------------------------
-NUMERATOR                EQU 0x30
-NUMERATOR2               EQU 0x31
-QUOTIENT                 EQU 0x40
-QUOTIENT2                EQU 0x41
-TEN                      EQU 10
+Numerator_1              EQU 0x30
+Numerator_2              EQU 0x31
+Quotient_1               EQU 0x40
+Quotient_2               EQU 0x41
+Subtractor_Ten           EQU 10
 
 ;----------------------------------
-; Decimal digit registers
+; Decimal Digit Registers (Hex-Dec Conversion Specific)
 ;----------------------------------
-refTempDecimalLOW        EQU 0x60   ; ones digit
-refTempDecimalMID        EQU 0x61   ; tens digit
-refTempDecimalHIGH       EQU 0x62   ; hundreds digit
+refTemp_Decimal_Low        EQU 0x60   ; One's Place
+refTemp_Decimal_High       EQU 0x61   ; Ten's Place
+refTemp_Decimal_Upper      EQU 0x62   ; Hundred's Place
 
-measuredTempDecimalLOW   EQU 0x70   ; ones digit
-measuredTempDecimalMID   EQU 0x71   ; tens digit
-measuredTempDecimalHIGH  EQU 0x72   ; hundreds digit
+measuredTemp_Decimal_Low   EQU 0x70   ; One's Place
+measuredTemp_Decimal_High  EQU 0x71   ; Ten's Place
+measuredTemp_Decimal_Upper EQU 0x72   ; Hundred's Place
 
 ;----------------------------------
 ; MAIN PROGRAM
@@ -87,14 +88,14 @@ PSECT absdata,abs,ovrld
     ORG 0x20
 
 ;----------------------------------
-; Clear PORTD and make it output
+; Clear PORTD & Enable It's Ports As Output
 ;----------------------------------
     CLRF    PORTD
     MOVLW   0x00
     MOVWF   TRISD, A
 
 ;----------------------------------
-; Put input values in registers
+; Placing Input Values In Registers
 ;----------------------------------
     MOVLW   measuredTempInput
     MOVWF   measuredTempREG
@@ -105,174 +106,173 @@ PSECT absdata,abs,ovrld
     CLRF    contREG
 
 ;----------------------------------
-; Check ref temp is from 10 to 50
-; This allows 10 and 50 too
+; Range Check ref temp Is From 10>= to <=50
 ;----------------------------------
 _refcheck:
     MOVLW   0x0A
     CPFSEQ  refTempREG
-    GOTO    _checkRefLow
-    GOTO    _checkRefHigh
+    GOTO    _check_Ref_Low
+    GOTO    _check_Ref_High
 
-_checkRefLow:
+_check_Ref_Low:
     MOVLW   0x0A
     CPFSGT  refTempREG
     SLEEP
 
-_checkRefHigh:
+_check_Ref_High:
     MOVLW   0x32
     CPFSEQ  refTempREG
-    GOTO    _checkRefTop
-    GOTO    _signcheck
+    GOTO    _check_Ref_Top
+    GOTO    _sign_check
 
-_checkRefTop:
+_check_Ref_Top:
     MOVLW   0x32
     CPFSLT  refTempREG
     SLEEP
 
-    GOTO    _signcheck
+    GOTO    _sign_check
 
 ;----------------------------------
-; If measured temp is negative,
-; go to heat right away
+; If measured temp is -ve,
+; go to heat straightaway
 ;----------------------------------
-_signcheck:
+_sign_check:
     BTFSC   measuredTempREG, 7
-    GOTO    _HEAT
-    GOTO    _equal
+    GOTO    _Heat
+    GOTO    _Equal
 
 ;----------------------------------
 ; Check if temps are equal
 ;----------------------------------
-_equal:
+_Equal:
     MOVF    refTempREG, 0
     CPFSEQ  measuredTempREG
-    GOTO    _comparison
-    GOTO    _NOTHING
+    GOTO    _Comparison
+    GOTO    _Do_Nothing
 
 ;----------------------------------
 ; If not equal, choose heat or cool
 ;----------------------------------
-_comparison:
+_Comparison:
     MOVF    refTempREG, 0
     CPFSGT  measuredTempREG
-    GOTO    _HEAT
-    GOTO    _COOL
+    GOTO    _Heat
+    GOTO    _Cool
 
 ;----------------------------------
 ; Too hot
 ; contREG = 2
 ; turn on PORTD.2
 ;----------------------------------
-_COOL:
+_Cool:
     MOVLW   0x02
     MOVWF   contREG
 
     MOVLW   0b00000100
     MOVWF   PORTD, A
 
-    GOTO    _measuredDECIMAL
+    GOTO    _measured_Decimal
 
 ;----------------------------------
 ; Too cold
 ; contREG = 1
 ; turn on PORTD.1
 ;----------------------------------
-_HEAT:
+_Heat:
     MOVLW   0x01
     MOVWF   contREG
 
     MOVLW   0b00000010
     MOVWF   PORTD, A
 
-    GOTO    _measuredDECIMAL
+    GOTO    _measured_Decimal
 
 ;----------------------------------
 ; Temps are same
 ; contREG = 0
 ; turn off output
 ;----------------------------------
-_NOTHING:
+_Do_Nothing:
     MOVLW   0x00
     MOVWF   contREG
 
     MOVLW   0b00000000
     MOVWF   PORTD, A
 
-    GOTO    _measuredDECIMAL
+    GOTO    _measured_Decimal
 
 ;----------------------------------
 ; Split measured temp into digits
 ;----------------------------------
-_measuredDECIMAL:
+_measured_Decimal:
     MOVF    measuredTempREG, 0
-    MOVWF   NUMERATOR
+    MOVWF   Numerator_1
 
-    MOVLW   TEN
-    CLRF    QUOTIENT
+    MOVLW   Subtractor_Ten
+    CLRF    Quotient_1
 
 _D1:
-    INCF    QUOTIENT
-    SUBWF   NUMERATOR
+    INCF    Quotient_1
+    SUBWF   Numerator_1
     BC      _D1
 
-    ADDWF   NUMERATOR
-    DECF    QUOTIENT
+    ADDWF   Numerator_1
+    DECF    Quotient_1
 
-    MOVFF   NUMERATOR, measuredTempDecimalLOW
-    MOVFF   QUOTIENT, NUMERATOR
-    CLRF    QUOTIENT
+    MOVFF   Numerator_1, measuredTemp_Decimal_Low
+    MOVFF   Quotient_1, Numerator_1
+    CLRF    Quotient_1
 
 _D2:
-    INCF    QUOTIENT
-    SUBWF   NUMERATOR
+    INCF    Quotient_1
+    SUBWF   Numerator_1
     BC      _D2
 
-    ADDWF   NUMERATOR
-    DECF    QUOTIENT
+    ADDWF   Numerator_1
+    DECF    Quotient_1
 
-    MOVFF   NUMERATOR, measuredTempDecimalMID
-    MOVFF   QUOTIENT, measuredTempDecimalHIGH
+    MOVFF   Numerator_1, measuredTemp_Decimal_High
+    MOVFF   Quotient_1, measuredTemp_Decimal_Upper
 
-    GOTO    _refDECIMAL
+    GOTO    _ref_Decimal
 
 ;----------------------------------
 ; Split ref temp into digits
 ;----------------------------------
-_refDECIMAL:
+_ref_Decimal:
     MOVF    refTempREG, 0
-    MOVWF   NUMERATOR2
+    MOVWF   Numerator_2
 
-    MOVLW   TEN
-    CLRF    QUOTIENT2
+    MOVLW   Subtractor_Ten
+    CLRF    Quotient_2
 
 _D11:
-    INCF    QUOTIENT2
-    SUBWF   NUMERATOR2
+    INCF    Quotient_2
+    SUBWF   Numerator_2
     BC      _D11
 
-    ADDWF   NUMERATOR2
-    DECF    QUOTIENT2
+    ADDWF   Numerator_2
+    DECF    Quotient_2
 
-    MOVFF   NUMERATOR2, refTempDecimalLOW
-    MOVFF   QUOTIENT2, NUMERATOR2
-    CLRF    QUOTIENT2
+    MOVFF   Numerator_2, refTemp_Decimal_Low
+    MOVFF   Quotient_2, Numerator_2
+    CLRF    Quotient_2
 
 _D22:
-    INCF    QUOTIENT2
-    SUBWF   NUMERATOR2
+    INCF    Quotient_2
+    SUBWF   Numerator_2
     BC      _D22
 
-    ADDWF   NUMERATOR2
-    DECF    QUOTIENT2
+    ADDWF   Numerator_2
+    DECF    Quotient_2
 
-    MOVFF   NUMERATOR2, refTempDecimalMID
-    MOVFF   QUOTIENT2, refTempDecimalHIGH
+    MOVFF   Numerator_2, refTemp_Decimal_High
+    MOVFF   Quotient_2, refTemp_Decimal_Upper
 
 ;----------------------------------
-; Stay here at the end
+; Program End
 ;----------------------------------
 _END:
-    GOTO    _END     // Add Breakpoint Here
+    GOTO    _END     // Break_Point
 
     END
