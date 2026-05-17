0840996ec0402c4e7c5c5702a4ca9c0005012aeb
ChrisDavid2311 <davidrajaa@sonoma.edu>
2026-05-16 22:40:19 -0700

    Create Random_Num_Gen_1-100.c

---
+++ Assignments/UART/Random_Num_Gen_1-100.c
@@ -0,0 +1,41 @@
+// ============================================
+// Title:   UART Function RAND
+// Author:  Christy Sahayaraj
+// Date:    05/16/2026
+// Version: 1
+// Purpose: Random Number Generation (1-100) Every 1 Second
+// Outcome: Generating Random Number B/W 1 to 100 Every 1 Second
+//============================================
+
+#include "mcc_generated_files/system/system.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int random_number(int min_num, int max_num)
+{
+    return (rand() % (max_num - min_num + 1)) + min_num;
+}
+
+int main(void)
+{
+    SYSTEM_Initialize();
+
+    UART2_Initialize();
+
+    // Seed random generator
+    srand(1234);
+
+    while(1)
+    {
+        int value = random_number(1, 100);
+
+        // Print only the random value
+        printf("%d\r\n", value);
+
+        // Toggle LED on RB1
+        PORTBbits.RB1 ^= 1;
+
+        // 1 second delay
+        __delay_ms(1000);
+    }
+}
